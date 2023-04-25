dcd189230f336a22c78ee54d0d93c60680477838
jt <454193896@qq.com>
2023-04-26 00:09:05 +0800

    4-26

--- Code/test.cpp
+++ Code/test.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
+class A {
+	public:
+	virtual void test() {}
+	A(){}
+	int a;
+};
+class B : public  A{
+	public:
+	B() {}
+};
 int main() {
-	
+	// B b;
+	// A* a = dynamic_cast<A*>(&b);
+
+	A a;
+	B* b = dynamic_cast<B*>(&a);
+	cout << b;
 	return 0;
 }
